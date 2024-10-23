c72742992277d4c7b92092164ee3dde8a66458a9
grezisek1 <grezisek@gmail.com>
2024-10-23 20:49:00 +0200

    refactor data structure, switch to procedural, add player movement

--- survive-the-monsters.js
+++ survive-the-monsters.js
@@ -1,41 +1,58 @@
 import * as Phaser from "./node_modules/phaser/dist/phaser.esm.js";
 
-const GRAVITY = 200;
+const movementConfig = {
+    speed: 300,
+};
+
+const GRAVITY = 0;
 const SCENE_WIDTH = 1920;
 const SCENE_HEIGHT = 1080;
 const SPRITE_SIZE = 128;
+const ENEMIES_COUNT = 2;
 
-const actors = {};
+const actors = {
+    enemies: new Array(ENEMIES_COUNT),
+};
 
-class GameScene extends Phaser.Scene {
-    preload () {
-        this.load.image("player", "sprites/players/mvp_player/idle_0.png");
-        this.load.image("monster_0", "sprites/monsters/mvp_monster_0/idle_0.png");
-        this.load.image("monster_1", "sprites/monsters/mvp_monster_1/idle_0.png");
+const sceneCenterX = SCENE_WIDTH / 2;
+const sceneCenterY = SCENE_HEIGHT / 2;
+const enemiesX = new Float64Array(ENEMIES_COUNT);
+const enemiesY = new Float64Array(ENEMIES_COUNT);
+const input = [0, 0];
+let playerX = 0;
+let playerY = 0;
+let inputKeys;
+
+const scenes = {
+    main: new Phaser.Scene("MainScene"),
+};
+scenes.main.preload = function() {
+    this.load.image("player", "sprites/players/mvp_player/idle_0.png");
+    this.load.image("monster_0", "sprites/monsters/mvp_monster_0/idle_0.png");
+    this.load.image("monster_1", "sprites/monsters/mvp_monster_1/idle_0.png");
+};
+scenes.main.create = function() {
+    actors.player = this.physics.add.image(SPRITE_SIZE, SPRITE_SIZE, "player");
+    for (let enemyId = 0; enemyId < ENEMIES_COUNT; enemyId++) {
+        actors.enemies[enemyId] = this.physics.add.image(SPRITE_SIZE, SPRITE_SIZE, `monster_${enemyId}`);
     }
-    create () {
-        actors.player = this.physics.add.image(SPRITE_SIZE, SPRITE_SIZE, "player");
-        actors.player.setVelocity(100, 0);
-        actors.player.setBounce(1, 1);
-        actors.player.setCollideWorldBounds(true);
+    
+    actors.player.setPosition(sceneCenterX, sceneCenterY);
 
-        actors.monster_0 = this.physics.add.image(SPRITE_SIZE, SPRITE_SIZE, "monster_0");
-        actors.monster_0.setVelocity(-150, 10);
-        actors.monster_0.setBounce(1, 1);
-        actors.monster_0.setCollideWorldBounds(true);
-        
-        actors.monster_1 = this.physics.add.image(SPRITE_SIZE, SPRITE_SIZE, "monster_1");
-        actors.monster_1.setVelocity(-200, 0);
-        actors.monster_1.setBounce(1, 1);
-        actors.monster_1.setCollideWorldBounds(true);
-    }
-}
+    this.physics.world.on("worldstep", physicsLoop);
+    inputKeys = this.input.keyboard.addKeys({
+        up: "W",
+        down: "S",
+        left: "A",
+        right: "D",
+    });
+};
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: SCENE_WIDTH,
     height: SCENE_HEIGHT,
-    scene: GameScene,
+    scene: scenes.main,
     physics: {
         default: "arcade",
         arcade: {
@@ -43,3 +60,35 @@ const game = new Phaser.Game({
         }
     }
 });
+
+function physicsLoop(dt) {
+    updateInput();
+    normalizeInput();
+    applyMovement(dt);
+    updatePositions();
+}
+function updateInput() {
+    input[0] = Number(inputKeys.right.isDown) - Number(inputKeys.left.isDown);
+    input[1] = Number(inputKeys.down.isDown) - Number(inputKeys.up.isDown);
+}
+function normalizeInput() {
+    const mSq = input[0]**2 + input[1]**2;
+    if (mSq == 0) {
+        return;
+    }
+    const m = mSq**0.5;
+    input[0] /= m;
+    input[1] /= m;
+}
+function applyMovement(dt) {
+    const speed = movementConfig.speed * dt;
+    playerX += input[0] * speed;
+    playerY += input[1] * speed;
+}
+function updatePositions() {
+    for (let enemyId = 0; enemyId < ENEMIES_COUNT; enemyId++) {
+        const x = sceneCenterX - playerX + enemiesX[enemyId];
+        const y = sceneCenterY - playerY + enemiesY[enemyId];
+        actors.enemies[enemyId].setPosition(x, y);
+    }
+}
